390aa0cc2304b2cdd2add5b31844ef26bfec71ef
Baldeep Kang <bkang4612@gmail.com>
2020-09-14 16:57:11 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN default FALSE,
+    PRIMARY KEY (id)
+)
