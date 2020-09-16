b8178291ca3fc4de9ca7f0ca9f035fc3618e3ac2
Baldeep Kang <bkang4612@gmail.com>
2020-09-15 17:26:11 -0700

    add schema.sql for database and seed.sql for data

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES('McChicken', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Big Whopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES('Garden Whopper', false);
